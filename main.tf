aa3e814d9609768774cbf3e73ea02d38ef1d2d7f
umamanasa <umamanasa49@gmail.com>
2024-03-26 16:10:04 +0530

    vpc module creation

--- main.tf
+++ main.tf
@@ -46,3 +46,22 @@ EOF
 #  name              = each.value["name"]
 #  instance_type     = each.value["instance_type"]
 #}
+
+##main.tfvars -- expense teraform
+#components = {
+#  frontend = {
+#    name          = "frontend"
+#    instance_type = "t2.micro"
+#  }
+#  mysql = {
+#    name          = "mysql"
+#    instance_type = "t2.micro"
+#  }
+#  backend = {
+#    name          = "backend"
+#    instance_type = "t2.micro"
+#  }
+#}
+#
+#security_groups = [ "sg-041096a23e28b0eb0" ]
+#zone_id         = "Z0365188L7MG2LV8YN4J"
